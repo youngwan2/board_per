15648e53d7f763469ecca445e1243fdbe348235e
KYW <qodna25@gmail.com>
2023-06-09 14:01:32 +0900

    로그인 기능 추가(가짜 로그인 기능)

--- front/src/app.js
+++ front/src/app.js
@@ -1,21 +1,41 @@
+// (() => {
+/* 헤더 쿼리 */
 const categories = document.querySelector(".category_ul");
 const categoriesLi = document.querySelectorAll(".category_ul>li");
+
+/* 메인 쿼리 */
 const main = document.getElementById("main");
+
+/* 페이지 쿼리 */
 const homePage = document.querySelector(".home_page");
 const listPage = document.querySelector(".list_page");
 const writingPage = document.querySelector(".writing_page");
 const detailPage = document.querySelector(".detail_page");
 const updatePage = document.querySelector(".update_page");
+
+/* 글쓰기 쿼리 */
 const wUserInput = document.getElementById("text_title");
 const wContent = document.getElementById("writing_content");
 const wWriter = document.getElementById("writing_writer");
 const wImage = document.getElementById("writing_image");
 const wRegistBtn = document.getElementById("regist_btn");
+wWriter.value = JSON.parse(sessionStorage.getItem('user'))?.username
 
-let categoryIndex = 0;
-let currentItem = 0;
+/* 로그인 쿼리 */
+const username = document.getElementById('username')
+const password = document.getElementById('pw')
+const loginIcon = document.querySelector(".login_icon")
+loginIcon.textContent = 'login'
+
+/* 레이아웃 쿼리 */
+const layout = document.querySelector('.layout')
+
+/* 변수 */
+// [현재 카테고리의 인덱스, 현재 게시글의 각 인덱스] =[각각 카운트]
+let [categoryIndex, currentItem] = [0, 0]
+
+// 로컬에 데이터 존재 안 하면 undefined 이므로 || 뒤의 0 을 할당
 let listId = JSON.parse(localStorage.getItem("data"))?.length ?? 0;
-console.log(listId);
 
 /* 데이터베이스 */
 // 로컬에 이미 존재하는 데이터를 가져와서 database 변수에 할당
@@ -26,42 +46,50 @@ const database = localData;
 
 /* 데이터베이스에 데이터를 전송하는 함수 */
 function storePushFunc() {
+  const loginUsername = JSON.parse(sessionStorage.getItem('user'))
   const data = {
     id: listId++,
     title: wUserInput.value,
     content: wContent.value,
-    writer: wWriter.value,
+    writer: loginUsername,
     date: new Date().toLocaleString(),
-    image: "",
+    image: '',
+    count: 0
   };
 
   database.push(data);
-  const dbData = JSON.parse(localStorage.getItem("data")) || [];
-  const newDatabase = [...dbData];
-  console.log(newDatabase);
-
-  const json = JSON.stringify(database);
-  console.log(listId);
-  localStorage.setItem("data", json);
-
-  render(database);
+  reload();
 }
 
+
+/* 로그인 상태 관리 */
+const state = (() => {
+  function loginHandler(isLogin = false) {
+    const loginState = isLogin
+    return loginState
+  }
+
+  return loginHandler
+
+})()
+
+
+
+/* 메뉴(카테고리) 클릭 시 포커스 유지하는 함수 */
 function menuFocus(categoryIndex) {
   categoriesLi.forEach((_, i) => {
     categoriesLi[i].classList.remove("menu_on");
-    if (i === categoryIndex) {
-      categoriesLi[i].classList.add("menu_on");
-    }
+    if (i === categoryIndex) categoriesLi[i].classList.add("menu_on");
   });
-}
-
+};
 menuFocus(categoryIndex);
 
+/* 카테고리 클릭 시 페이지 이동 함수를 호출하는 이벤트 등록 */
 categories.addEventListener("click", (e) => {
   pageChange(e.target);
 });
 
+// 페이지을 처리하는 함수
 const pageChange = (category) => {
   const menu = category.dataset.id;
   switch (menu) {
@@ -93,31 +121,45 @@ const home = () => {
   homePage.style.cssText = `visibility:visible; opacity:1`;
   listPage.style.cssText = `visibility:hidden; opacity:0`;
   writingPage.style.cssText = `visibility:hidden; opacity:0`;
-  detailPage.style.cssText= `visibility:hidden; opacity:0`;
+  detailPage.style.cssText = `visibility:hidden; opacity:0`;
+  updatePage.style.cssText = `visibility:hidden; opacity:0`;
 };
 
-home()
+home();
 
 /* 게시판 페이지 */
 const board = () => {
   listPage.style.cssText = `visibility:visible; opacity:1`;
   homePage.style.cssText = `visibility:hidden; opacity:0`;
   writingPage.style.cssText = `visibility:hidden; opacity:0`;
-  detailPage.style.cssText= `visibility:hidden; opacity:0`;
+  detailPage.style.cssText = `visibility:hidden; opacity:0`;
+  updatePage.style.cssText = `visibility:hidden; opacity:0`;
 };
 
 /* 글쓰기 페이지 */
 const writing = () => {
+  const isLogin = JSON.parse(sessionStorage.getItem('user'))?.isLogin || false
+  if (!isLogin) {
+    alert("로그인 하세요")
+    return loginToggle();
+  }
   writingPage.style.cssText = `visibility:visible; opacity:1`;
   homePage.style.cssText = `visibility:hidden; opacity:0`;
   listPage.style.cssText = `visibility:hidden; opacity:0`;
-  detailPage.style.cssText= `visibility:hidden; opacity:0`;
+  detailPage.style.cssText = `visibility:hidden; opacity:0`;
+  updatePage.style.cssText = `visibility:hidden; opacity:0`;
 };
 
 /* 글수정 페이지 */
+const update = () => {
+  updatePage.style.cssText = `visibility:visible; opacity:1`;
+  writingPage.style.cssText = `visibility:hidden; opacity:0`;
+  homePage.style.cssText = `visibility:hidden; opacity:0`;
+  listPage.style.cssText = `visibility:hidden; opacity:0`;
+  detailPage.style.cssText = `visibility:hidden; opacity:0`;
+}
 
 /*  게시판으로 이동 */
-
 document.querySelectorAll(".move_btn").forEach((moveBtn) => {
   moveBtn.addEventListener("click", () => {
     board();
@@ -127,39 +169,31 @@ document.querySelectorAll(".move_btn").forEach((moveBtn) => {
 
 /* ---------------기능추가------------ */
 
+/* 글 등록 */
 // 글작성 페이지에서 등록 버튼을 클릭하면 입력한 데이터를 데이터베이스에 추가한다.
 wRegistBtn.addEventListener("click", () => {
   storePushFunc();
   board();
   menuFocus(1);
+
+  [wUserInput.value, wContent.value, wWriter.value] = ['', '', '']
 });
 
 /* 게시판 렌더 함수 */
-const thText = ["글번호", "제목", "작성자", "조회수", "작성일자"];
 const render = () => {
   let listHTML = "";
 
   listHTML += `<tr><th>글번호</th><th>제목</th><th>작성자</th><th>조회수</th><th>작성일자</th></tr>
   `;
 
-  // listHTML += database.map((data, i) => {
-  //   return `
-  //   <tr>
-  //       <td>${data.id}</td>
-  //       <td>${data.title}</td>
-  //       <td>${data.writer}</td>
-  //       <td><span>10<span></td>
-  //       <td>${data.date}</td>
-  //   </tr>`;
-  // });
   console.log(database.length);
   for (let i = database.length - 1; i >= 0; i--) {
     listHTML += `
       <tr onclick="detail(${i})">
-          <td>${database[i].id}</td>
+          <td>${1 * database[i].id + 1}</td>
           <td>${database[i].title}</td>
           <td>${database[i].writer}</td>
-          <td><span>10<span></td>
+          <td><span>${database[i].count}<span></td>
           <td>${database[i].date}</td>
       </tr>
     `;
@@ -167,18 +201,20 @@ const render = () => {
   document.getElementById("list_table").innerHTML = listHTML;
 };
 
-/* 상세내용 페이지 */
+/* 상세내용 페이지 렌더링 함수 */
 const detail = (i) => {
   currentItem = i;
+  database[i].count++
   detailPage.style.cssText = `visibility:visible; opacity:1`;
   writingPage.style.cssText = `visibility:hidden; opacity:0`;
   homePage.style.cssText = `visibility:hidden; opacity:0`;
   listPage.style.cssText = `visibility:hidden; opacity:0`;
+  reload();
 
   const detailHTML = `
-<p><span>글번호</span>${database[i].id}</p>
+<p><span>글번호</span>${1 * database[i].id + 1}</p>
 <p><span>제목</span>${database[i].title}</p>
-<p><span>조회수</span>10005</p>
+<p><span>조회수</span>${database[i].count}</p>
 <p><span>작성자</span>${database[i].writer} </p>
 <p><span>작성일시</span>${database[i].date}</p>
 <p><span>내용</span>${database[i].content}</p>
@@ -193,6 +229,26 @@ const detail = (i) => {
   document.querySelector(".detail_content_area").innerHTML = detailHTML;
 };
 
+/* 글 수정 함수 */
+function updateFunc() {
+  const title = document.getElementById('update_input');
+  const content = document.getElementById('update_content');
+  database[currentItem].title = title.value;
+  database[currentItem].content = content.value;
+  reload();
+  board();
+
+  [title.value, content.value] = ['', '']
+}
+
+/* 글 삭제 함수 */
+function deleteFunc() {
+  database.splice(currentItem, 1)
+  reload();
+  board();
+}
+
+/* 이전 게시글 이동 */
 function prevBtn() {
   if (currentItem <= 0) {
     return (currentItem = 0);
@@ -202,13 +258,133 @@ function prevBtn() {
   detail(currentItem);
 }
 
+/* 다음 게시글 이동 */
 function nextBtn() {
   if (database.length - 1 > currentItem) currentItem++;
   console.log(currentItem);
   detail(currentItem);
 }
 
+
+// 로그인 버튼 클릭 시 초기화 방지
+document.querySelector('.login_form').addEventListener('submit', (e) => {
+  e.preventDefault();
+})
+
+
+/* 로그인 */
+function login() {
+  const userInfo = {
+    isLogin: state(true),
+    username: username.value,
+    password: password.value
+  };
+
+  const user = JSON.stringify(userInfo);
+  sessionStorage.setItem('user', user);
+
+  // input 초기화
+  [userInfo.username, userInfo.password] = ['', ''];
+
+  const isLogin = JSON.parse(sessionStorage.getItem('user')).isLogin || 'login';
+  if (isLogin) {
+    sessionStorage.setItem('login', 'logout');
+  }
+
+  loginMessage();
+
+  home()
+  menuFocus(0)
+
+};
+
+/* 로그아웃 처리 */
+function logout() {
+  sessionStorage.removeItem('user') // 세션 저장소 유저 정보 제거
+  sessionStorage.setItem('login', 'login'); // 로그인 메시지 업데이트
+  loginIcon.innerHTML = sessionStorage.getItem('login'); // 이하동문
+
+  home()
+  menuFocus(0)
+};
+
+function loginMessage() {
+  document.querySelector('.login_icon').textContent = sessionStorage.getItem('login');
+}
+
+
+// loginMessage()
+
+
+/* 로그인 토글  */
+function loginToggle() {
+  document.querySelector('.login_form').classList.toggle('login_on')
+  layout.classList.toggle('layout_on')
+}
+
+/* 이벤트 리스너 등록 */
 document.querySelector(".prev_btn").addEventListener("click", prevBtn);
 document.querySelector(".next_btn").addEventListener("click", nextBtn);
+document.querySelector(".update_btn").addEventListener("click", update); // 수정 페이지 이동
+document.querySelector('.update').addEventListener("click", updateFunc)
+document.querySelector(".del_btn").addEventListener("click", deleteFunc)
+document.querySelector(".cancle_btn").addEventListener("click", board)
+document.querySelector(".writing_btn").addEventListener("click", writing)
+document.querySelector(".home_logo").addEventListener("click", () => {
+  home()
+  menuFocus(0)
+})
+
+/* 로그인 아이콘 클릭 시 해당 아이콘 텍스트가 login 이라면 login 모달창을 띄운다.  */
+loginIcon.addEventListener('click', () => {
+  if (loginIcon.textContent === "login") loginToggle()
+  try {
+    // 유저가 로그인한 상태라면 logout 을 실행한다.
+    /* 세션에 데이터가 없다면, null 을 isLogin에 저장한다. */
+    const isLogin = JSON.parse(sessionStorage.getItem('user'))?.isLogin;
+
+    // null 이면 뒤에 것을 실행하지 않는다. true 라면 뒤에 함수를 실행한다.
+    isLogin && logout()
+  } catch (error) {
+    console.log(error + '는 넘어가도 됩니다.')
+  }
+  /* isLogin이 false 라면 실행 x , true 라면 logout() 호출 */
+  if (sessionStorage.getItem('login') === 'logout') {
+    sessionStorage.setItem('login', 'login')
+  }
+})
+
+// 로그인 정규식 검사 함수
+function loginRegex() {
+  const pwRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+  const unRegex = /[a-zA-Z0-9]{4,}/
+  const pwJugement = pwRegex.test(password.value)
+  const unJugement = unRegex.test(username.value)
+
+  // 닉네임과 패스워드 정규식 검사가 모두 참이라면 login 처리
+  if (unJugement && pwJugement) {
+    login()
+    loginToggle()
+  }
+}
+
+document.querySelector('.layout').addEventListener('click', loginToggle);
+document.querySelector('.login_sumbit_btn').addEventListener('click', () => {
+  loginRegex() // 정규식 검사 실시
+});
 
 render();
+
+
+function reload() {
+  const json = JSON.stringify(database);
+  console.log(listId);
+  localStorage.setItem("data", json);
+  render(database)
+}
+// })()
+
+
+
+const reader = new FileReader();
+// console.log(reader.readAsText(wImage.file[0]))
